dd47f8d0ce4c75c4d04d249805292dc94fa670f5
LidijaPu <lidijapusciute@gmail.com>
2024-09-23 21:20:24 +0300

    pridėta Studentas struktūra

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+struct Studentas {
+    string vardas;
+    string pavarde;
+    vector<int> namu_darbai;
+    int egzaminas;
+};
+
+int main() {
+  
+      vector<Studentas> studentai;
+      int n, nd_kiekis;
+
+
+
+ return 0;
+}
