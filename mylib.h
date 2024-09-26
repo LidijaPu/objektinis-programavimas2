1260a74ab9cc7846315540eeba16dbcc74a2d1eb
LidijaPu <lidijapusciute@gmail.com>
2024-09-26 16:06:00 +0300

    mylib.h

---
+++ mylib.h
@@ -0,0 +1,16 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <numeric>
+#include <algorithm>
+#include <limits>
+#include <stdexcept> 
+
+using namespace std;
+
+#endif
