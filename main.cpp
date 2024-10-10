0c55ee0a58bcde57d697abeb94d665b648f70311
LidijaPu <lidijapusciute@gmail.com>
2024-10-10 20:51:14 +0300

    pakeistas 3 pasirinkimas: generuoja studentu sąrašus

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main() {
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
         
-    if (pasirinkimas == 1 || pasirinkimas == 3) {
+    if (pasirinkimas == 1) {
         cout << "Iveskite studentu skaiciu: ";
         while (!(cin >> n) || n <= 0) {
             cout << "Klaida: Prasome ivesti tik skaiciu: ";
@@ -27,29 +27,32 @@ int main() {
             cout << "Iveskite studento " << i + 1 << " pavarde: ";
             cin >> s.pavarde;
         
-            if (pasirinkimas == 1) {
-                cout << "Iveskite namu darbu balus (noredami baigti ivesti, iveskite neigiama skaiciu):" << endl;
-                while (true) {
-                    int rezultatas;
-                    cout << "Namu darbu balas: ";
-                    cin >> rezultatas;
-                    if (rezultatas < 0) break;
-                    if (rezultatas >= 1 && rezultatas <= 10) s.namu_darbai.push_back(rezultatas);
-                    else cout << "Klaida: Prasome ivesti skaiciu tarp 1 ir 10." << endl;
-                }
-                cout << "Iveskite egzamino bala: ";
-                while (!(cin >> s.egzaminas) || s.egzaminas < 1 || s.egzaminas > 10) {
-                    cout << "Klaida: Prasome ivesti skaicių nuo 1 iki 10: ";
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                }
+            cout << "Iveskite namu darbu balus (noredami baigti ivesti, iveskite neigiama skaiciu):" << endl;
+            while (true) {
+                int rezultatas;
+                cout << "Namu darbu balas: ";
+                cin >> rezultatas;
+                if (rezultatas < 0) break;
+                if (rezultatas >= 1 && rezultatas <= 10) s.namu_darbai.push_back(rezultatas);
+                else cout << "Klaida: Prasome ivesti skaiciu tarp 1 ir 10." << endl;
             }
-            else {
-                generuotiAtsitiktiniusRezultatus(s);
-            }
+            cout << "Iveskite egzamino bala: ";
+            while (!(cin >> s.egzaminas) || s.egzaminas < 1 || s.egzaminas > 10) {
+                cout << "Klaida: Prasome ivesti skaicių nuo 1 iki 10: ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                }
             studentai.push_back(s);
         }
-        
+        int galutinis;
+        cout << "Ar norite skaiciuoti galutini bala pagal vidurki (1) ar mediana (2)? ";
+        while (!(cin >> galutinis) || (galutinis != 1 && galutinis != 2)) {
+            cout << "Klaida: Prasome ivesti 1 arba 2: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+        isvestis(studentai, galutinis); 
     }
     else if (pasirinkimas == 2) {
         string failoPavadinimas;
@@ -58,6 +61,7 @@ int main() {
         nuskaitytiIsFailo(failoPavadinimas, studentai);
     }
     
+    int galutinis;
     cout << "Ar norite skaiciuoti galutini bala pagal vidurki (1) ar mediana (2)? ";
     while (!(cin >> pasirinkimas) || (pasirinkimas != 1 && pasirinkimas != 2)) {
         cout << "Klaida: Prasome ivesti 1 arba 2: ";
@@ -65,7 +69,33 @@ int main() {
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
     rusiavimas(studentai);
-    isvestis(studentai, pasirinkimas);
+    isvestis(studentai, galutinis);
+
+}
+
+else if (pasirinkimas == 3) {
+    vector<int> kiekiai = { 1000, 10000, 100000, 1000000, 10000000 };
+
+    cout << "Ar norite skaiciuoti galutini bala pagal vidurki (1) ar mediana (2)? ";
+    while (!(cin >> pasirinkimas) || (pasirinkimas != 1 && pasirinkimas != 2)) {
+        cout << "Klaida: Prasome ivesti 1 arba 2: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int rusiuotiPagal;
+    cout << "Ar norite rusiuoti studentus pagal varda (1), pavarde (2) ar pazymi (3)\n? ";
+    while (!(cin >> rusiuotiPagal) || (rusiuotiPagal < 1 || rusiuotiPagal > 3)) {
+        cout << "Klaida: Prasome ivesti 1, 2 arba 3: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    for (int kiekis : kiekiai) {
+        string failoPavadinimas = "studentai_" + to_string(kiekis) + ".txt";
+        matuotiVeikimoGreiti(failoPavadinimas, kiekis, pasirinkimas, rusiuotiPagal);  // Keturi parametrai: failoPavadinimas, kiekis, pasirinkimas, rusiuotiPagal
+    }
+}
 
 return 0;
 }
