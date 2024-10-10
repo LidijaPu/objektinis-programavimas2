5aa6da35cc91c76e69c6b6505dcb87f8cb50b6f4
LidijaPu <lidijapusciute@gmail.com>
2024-10-10 21:00:15 +0300

    Create failai.h

---
+++ failai.h
@@ -0,0 +1,22 @@
+#ifndef FAILAI_H_INCLUDED
+#define FAILAI_H_INCLUDED
+
+#include "mylib.h"
+#include "stud.h"
+
+
+void generuotiStudentuDuomenis(const string& failoPavadinimas, int kiekis);
+void rusiuotiStudentus(const string& failoPavadinimas, int pasirinkimas, int rusiuotiPagal, const string& filePrefix);
+void surusioti_failai(vector<Studentas>& studentai, const string& failoPavadinimas);
+bool palyginimas_vardas(const Studentas& a, const Studentas& b);
+void rusiavimas_vardas(vector<Studentas>& studentai);
+bool palyginimas_pavarde(const Studentas& a, const Studentas& b);
+void rusiavimas_pavarde(vector<Studentas>& studentai);
+bool palyginimas_pazimys(const Studentas& a, const Studentas& b);
+void rusiavimas_pazimys(vector<Studentas>& studentai);
+void matuotiVeikimoGreiti(const string& failoPavadinimas, int kiekis, int pasirinkimas, int rusiuotiPagal);
+
+
+
+
+#endif
