fe1953fa4c26ff4103d6e93570c5459570b57658
LidijaPu <lidijapusciute@gmail.com>
2024-10-24 15:43:13 +0300

    nuskaitymas listai.cpp

--- listai.cpp
+++ listai.cpp
@@ -37,3 +37,57 @@ void isvestisList(const list<Studentas>& studentaiList, int pasirinkimas) {
         cout << endl;
     }
 }
+
+void nuskaitytiIsFailoList(const string& name, list<Studentas>& studentaiList, int pasirinkimas) {
+    ifstream fin;
+
+    try {
+        fin.open(name);
+        if (!fin.is_open()) {
+            throw runtime_error("Nepavyko atidaryti failo.");
+        }
+    }
+    catch (const runtime_error& e) {
+        cerr << e.what() << endl;
+        return;
+    }
+
+    try {
+        string ignore;
+        getline(fin, ignore);
+        string line;
+
+        while (getline(fin, line)) {
+            istringstream iss(line);
+            Studentas sl;
+            string vardas, pavarde;
+
+            if (!(iss >> vardas >> pavarde)) {
+                cerr << " " << endl;
+                continue;
+            }
+
+            sl.vardas = vardas;
+            sl.pavarde = pavarde;
+
+            int pazymys;
+            vector<int> pazymiai;
+            while (iss >> pazymys) {
+                pazymiai.push_back(pazymys);
+            }
+
+            if (!pazymiai.empty()) {
+                sl.egzaminas = pazymiai.back();
+                pazymiai.pop_back();
+                sl.namu_darbai = pazymiai;
+            }
+
+            studentaiList.push_back(sl);
+        }
+
+        fin.close();
+    }
+    catch (const ifstream::failure& e) {
+        cerr << "Nepavyko nuskaityti failo: " << e.what() << endl;
+    }
+}
