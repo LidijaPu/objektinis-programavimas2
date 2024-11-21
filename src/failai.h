b253ba5c7e4e258c9d4a4747a5c20d091afb61a0
LidijaPu <lidijapusciute@gmail.com>
2024-11-21 12:57:29 +0200

    Update failai.h

--- src/failai.h
+++ src/failai.h
@@ -6,7 +6,6 @@
 
 
 void generuotiStudentuDuomenis(const string& failoPavadinimas, int kiekis);
-void nuskaitytiStudentus(const string& failoPavadinimas, vector<Studentas>& studentai, int pasirinkimas);
 void rusiuotiStudentus(vector<Studentas>& studentai, int pasirinkimas, int rusiuotiPagal, int strategija,
     vector<Studentas>& vargsiukai, vector<Studentas>& kietiakiai);
 void surusioti_failai(vector<Studentas>& studentai, const string& failoPavadinimas);
@@ -18,8 +17,6 @@ bool palyginimas_pazimys(const Studentas& a, const Studentas& b);
 void rusiavimas_pazimys(vector<Studentas>& studentai);
 void matuotiVeikimoGreiti(const string& failoPavadinimas, int kiekis, int pasirinkimas, int rusiuotiPagal, int strategija);
 void atmintis(const vector<Studentas>& v, const string& name);
-
-
 
 
 #endif
